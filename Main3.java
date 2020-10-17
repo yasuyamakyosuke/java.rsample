d2628748bc02979a90e469cf61534ecf4dec10c1
Yasuyamakyosuke <toyojindai@gmail.com>
2020-10-17 19:54:36 +0900

    Main3_finish

---
+++ Main3.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("名前：");
+    // 文字列の入力を受け取ってください
+    String firstName = 
+
+    System.out.print("名字：");
+    // 文字列の入力を受け取ってください
+    String lastName = 
+
+    System.out.print("年齢：");
+    // 整数の入力を受け取ってください
+    int age = 
+
+    System.out.print("身長(m)：");
+    // 小数の入力を受け取ってください
+    double height = 
+
+    System.out.print("体重(kg)：");
+    // 小数の入力を受け取ってください
+    double weight = 
+
+    Person.printData(Person.fullName(firstName, lastName), age, height, weight);
+  }
+}
